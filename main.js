d701c562d1ab367ef60ffa1660b9f7e29cd440af
Jaylon <Jaylonmcc@icloud.com>
2022-06-14 20:24:03 -0500

    Added game functionality and UI

--- main.js
+++ main.js
@@ -1,61 +1,116 @@
-function computerPLay () { 
-    // this variable chooses a random number from 0.1 - 0.99
-    const random = Math.random()
-    // If the random we generated is less then 0.34 then the value is 'rock'
-    if(random < 0.34) {
-        return 'rock';
-        //If the random number we generated is less than or equal to 0.67 
-    } else if(random <= 0.67) {
-        return 'paper';
-    } else {
-        return 'scissors'
-    }
+/* Scoring for user and computer */
+let computerScore = 0;
+let playerScore = 0;
+
+/* Storing variables for future use */
+const scoreInfo = document.getElementById("winnerofthisround")
+const scoreMessage = document.getElementById('scoreMessage')
+let playersSign = document.getElementById('playersSign')
+let computersSign = document.getElementById('computersSign')
+const playerScores = document.getElementById('playerScores')
+const computerScores = document.getElementById('computerScores')
+const rockBtn = document.getElementById('rockBtn')
+const paperBtn = document.getElementById('paperBtn')
+const scissorBtn = document.getElementById('scissorsBtn')
+let winnerofthisround = document.getElementById('winnerofthisround')
+
+
+
+/* How the computer selects */
+function getComputerChoice () {
+  const choices = ["r", "p", "s"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+
+}
+/* What happens when the Player wins */
+function win(userChoice, computerChoice) {
+ playerScore++
+ playerScores.textContent = `Player :  ${playerScore}`
+ computerScores.textContent = `Computer : ${computerScore}`;
+ winnerofthisround.textContent = 'PLAYER WINS'
+ if (userChoice === 'r' && computerChoice == 's' ) {
+   scoreMessage.textContent = 'Rock beats Scissors :)'
+ }
+updateChoices(userChoice, computerChoice)
+ }
+ /*What happens when the computer wins */
+function lose(userChoice, computerChoice) {
+  computerScore++
+  playerScores.textContent = `Player : ${playerScore}`;
+  computerScores.textContent = `Computer : ${computerScore}`;
+  winnerofthisround.textContent = 'COMPUTER WINS'
+  updateChoices(userChoice, computerChoice)
+}
+/*What happens when it is a draw */
+function draw(userChoice, computerChoice) {
+  playerScores.textContent = `player : ${playerScore}`;
+  computerScores.textContent = `Computer : ${computerScore}`
+  winnerofthisround.textContent = 'DRAW HAHA'
+  updateChoices(userChoice, computerChoice)
+}
+/* What happens when the user selects the choices */
+function updateChoices (userChoice , computerChoice) {
+  switch (userChoice) {
+    case 'r':
+      playersSign.textContent = '✊';
+       break
+     case 'p':
+       playersSign.textContent = '✋';
+       break
+     case 's':
+       playersSign.textContent = '✌';
+       break}
+  switch (computerChoice) {
+    case 'r' :
+      computersSign.textContent = '✊'
+       break
+    case 'p':
+       computersSign.textContent = '✋'
+        break
+    case 's' :
+      computersSign.textContent = '✌'
+      break
+  }
 }
 
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === 'rock') {
-        if (computerSelection === 'rock'){
-            return 'tie'
-        } else if (computerSelection === 'paper') {
-            computerScore++;
-            return 'Computer Wins! Paper beats Rock'
-        } else {
-            playerScore++;
-            return 'player wins Rock beats Scissors'
-        }
-    }
-    if (playerSelection === 'paper') {
-        if (computerSelection === 'rock') {
-            playerScore++;
-            return 'Player wins, Paper beats rock'
-        } else if (computerSelection === 'Scissors') {
-            computerScore
-            return 'Computer wins, Scissor beats paper'
-        } else (computerSelection === 'paper')
-            return 'Tie'
-    }
-    if (playerSelection === 'scissors') {
-        if (computerSelection === "rock") {
-            computerScore++;
-            return 'Rock wins, Rock beats scissors'
-        } else if (computerSelection === 'paper') {
-            playerScore++ 
-            return 'Player wins, Scissors beats paper'
-        } else {
-            return tie;
-        }
-    }
-}
-playerScore = parseInt(0);
-computerScore = parseInt(0);
 
-function game() {
-    for (i = 0; i < 5; i++) {
-        let playerSelection = prompt("Pick a move, Rock, Paper or Scissors")
-        const computerSelection = computerPLay()
-        console.log(playRound(playerSelection, computerSelection))
-        console.log('Player Score = ' + playerScore);
-        console.log('Computer Score =' + computerScore)
-    }
+/* Taking the user choice  */
+function game(userChoice) {
+const computerChoice = getComputerChoice();
+/*If user wins */
+switch (userChoice + computerChoice) {
+  case "rs":
+  case "pr":
+  case "sp":
+      win(userChoice, computerChoice)
+     break;
+/* If computer wins */
+  case "rp":
+  case "ps":
+  case "sr":
+    lose(userChoice, computerChoice)
+     break;
+/* If it is a draw */
+  case "rr":
+  case "pp":
+  case "ss":
+    draw(userChoice, computerChoice)
+     break;
+} 
 }
-game()
+/* What happens when we click the rock paper scissor buttons */
+
+
+function main() {
+  rockBtn.addEventListener("click",function() {
+    game('r')
+   })
+   paperBtn.addEventListener("click", function() {
+     game('p')
+   })
+   scissorBtn.addEventListener("click", function() {
+     game('s')
+   })
+  }
+  main()
